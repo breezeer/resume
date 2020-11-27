6fe0d7f296a19b3bb841d52ec9ad26b64978ce98
Breeze <zbeady@outlook.com>
2020-11-27 19:53:07 +0800

    「2020/11/27 19:53:06」

--- auto_push.sh
+++ auto_push.sh
@@ -2,12 +2,13 @@
 now=$(date "+%Y/%m/%d %H:%M:%S")
 echo $now
 
+# 手动填写 commit 信息。
 read -p  "Please input commit comments:" msg
 
-# 执行 git 命令
+# 执行 git 命令。
 echo Start.
 git add -A && git commit -m "「$now」$msg" && git pull && git push
 echo OK!
 
-# 保留窗口
+# 保留窗口。
 read
